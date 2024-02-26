09f602bda29060ad75f163ab46017fe2de087484
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-02-27 00:09:09 +0700

    init project

--- Buoi1/Main.java
+++ Buoi1/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Chuc mung nam moi 2024 ");
+        String thongBao = "Xin chào";
+        String hoVaTen = "Cao Tran Trong Hieu";
+        String lop;
+        lop = "Lap trình java";
+        System.out.println("Thong bao:" + thongBao);
+        System.out.println("Ho & Ten:" + hoVaTen);
+        System.out.println("Lop:" + lop);
     }
 }
+// khái niệm về biến và khai báo biến
