e1c34694046702309cc48e7c7a76d985f67107d9
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-04-21 21:15:59 +0700

    java

--- test.java
+++ test.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class test {
 
    public static void main(String[] args) {
-      for (int i = 0; i < 1000; i++) {
-         System.out.println("Ngu toi trua doi thanh cong");
+      int n;
+      Scanner sc = new Scanner(System.in);
+
+      // Nhập và kiểm tra giá trị của n
+      do {
+         System.out.println("Nhap vao so nguyen duong n (0 < n < 100):");
+         n = sc.nextInt();
+      } while (n <= 0 || n >= 100);
+
+      System.out.println("Cac so chan tu 0 den " + n + " la:");
+
+      // In ra các số chẵn từ 0 đến n
+      for (int i = 0; i <= n; i += 2) {
+         System.out.println(i);
       }
    }
 }
