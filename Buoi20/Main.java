8ed61acbfa08fb77b1e84d2c4b5a0e6dbe83562f
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-05-30 10:06:19 +0700

    java

--- Buoi20/Main.java
+++ Buoi20/Main.java
@@ -10,5 +10,33 @@ public class Main {
         System.out.println("Nhan : " + vnc500.nhan(4, 5));
         System.out.println("Chia : " + vnc500.chia(4, 0));
         // -------------------------------------------------
+        System.out.println("--Test thuat toan--");
+        double[] arr = new double[] { 2, 1, 3, 4, 5, 6, 0 };
+        double[] arr2 = new double[] { 6, 2, 7, 9, 3, 4, 5 };
+        SapXepChen sxc = new SapXepChen();
+        SapXepChon sxchon = new SapXepChon();
+        System.out.println("--Sap xep tang--");
+        sxc.sapXepTang(arr);
+        for (int i = 0; i < arr.length; i++) {
+            double d = arr[i];
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("\n");
+        System.out.println("--Sap xem giam--");
+        sxchon.sapXepGiam(arr2);
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+        // --------------------------------------------
+        System.out.println("--\nTest phan mem may tinh---");
+        PhanMemMayTinh pmmt = new PhanMemMayTinh();
+        System.out.println("Cong : " + pmmt.cong(3, 5));
+        double[] arr3 = new double[] { 6, 2, 7, 9, 3, 4, 5 };
+        pmmt.sapXepTang(arr3);
+        for (int i = 0; i < arr.length; i++) {
+            double d = arr[i];
+            System.out.print(arr[i] + " ");
+        }
     }
+
 }
--- Buoi20/SapXepChon.java
+++ Buoi20/SapXepChon.java
@@ -4,12 +4,34 @@ public class SapXepChon implements SapXepInterface {
 
     @Override
     public void sapXepTang(double[] arr) {
+        int n = arr.length;
+        int i, j, min_inx;
+        for (i = 0; i < n - 1; i++) {
+            min_inx = i;
+            for (j = i + 1; j < n; j++)
+                if (arr[j] < arr[min_inx])
+                    min_inx = j;
+            double temp = arr[min_inx];
+            arr[min_inx] = arr[i];
+            arr[i] = temp;
 
+        }
     }
 
     @Override
     public void sapXepGiam(double[] arr) {
+        int n = arr.length;
+        int i, j, min_inx;
+        for (i = 0; i < n - 1; i++) {
+            min_inx = i;
+            for (j = i + 1; j < n; j++)
+                if (arr[j] > arr[min_inx])
+                    min_inx = j;
+            double temp = arr[min_inx];
+            arr[min_inx] = arr[i];
+            arr[i] = temp;
 
+        }
     }
 
 }
