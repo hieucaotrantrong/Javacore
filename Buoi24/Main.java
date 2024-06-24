d1f0e76f15d18826e9bff3ca29d63e21b4ece47f
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-06-24 15:33:01 +0700

    java

---
+++ Buoi24/Main.java
@@ -0,0 +1,56 @@
+package Buoi24;
+
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+/*
+ *Các lớp quản lý thời gian ngày tháng trong java
+ */
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * Hàm lấy thời gian hiện tại
+         */
+        long t1 = System.currentTimeMillis();
+        for (int i = 0; i < 1; i++) {
+            System.out.println("Test");
+        }
+        long t2 = System.currentTimeMillis();
+        System.out.println("Truoc khi chay for : " + t1);
+        System.out.println("Sau khi chay for : " + t2);
+        System.out.println("Thoi gian : " + ((t2 - t1)) + "mls");
+        System.out.println("Thoi gian : " + ((t2 - t1) / 1000) + "s");
+        /*
+         * TimeUnit
+         */
+
+        System.out.println("3000 nam = " + TimeUnit.DAYS.toSeconds(3000 * 365) + "s");
+        System.out.println("25h =" + TimeUnit.HOURS.toSeconds(25) + "s");
+        /*
+         * Date
+         */
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(date);
+        /*
+         * Calendar
+         */
+        Calendar time = Calendar.getInstance();
+        System.out.println(
+                time.get(Calendar.DATE) + "-" + (time.get(Calendar.MONTH) + 1) + "-" + time.get(Calendar.YEAR));
+        time.add(Calendar.HOUR, 30);
+        System.out.println(
+                time.get(Calendar.DATE) + "-" + (time.get(Calendar.MONTH) + 1) + "-" + time.get(Calendar.YEAR));
+        time.add(Calendar.DATE, 14);
+        System.out.println(
+                time.get(Calendar.DATE) + "-" + (time.get(Calendar.MONTH) + 1) + "-" + time.get(Calendar.YEAR));
+        /*
+         * DateDormat
+         */
+        DateFormat dateFormat = new SimpleDateFormat();
+        System.out.println(dateFormat.format(date));
+        dateFormat = new SimpleDateFormat("yyyy-MMMMM-dd hh:mm:aaa");
+    }
+}
--- Exemple22/Main.java
+++ Exemple22/Main.java
@@ -6,16 +6,16 @@ import java.util.Arrays;
   * Cách phân tách chuỗi thành mảng trang java
   */
 public class Main {
-    public static void main(String[] args) {
-        String name = "Xin chao cac ban, tui la hieu";
-        String[] name1 = name.split(" ");
-        System.out.println(Arrays.toString(name1));
-        String name2 = "Xin chao, minh la phuong.Minh la lap trinh vien!";
-        String[] name3 = name2.split("\\.|\\,");
-        System.out.println(Arrays.toString(name3));
-        String name4 = "Cao Tran Trong Hieu";
-        String[] name5 = name4.split(" ");
-        System.out.println(Arrays.toString(name5));
-        System.out.println("Ten : " + name5[name5.length - 1]);
-    }
+  public static void main(String[] args) {
+    String name = "Xin chao cac ban, tui la hieu";
+    String[] name1 = name.split(" ");
+    System.out.println(Arrays.toString(name1));
+    String name2 = "Xin chao, minh la phuong.Minh la lap trinh vien!";
+    String[] name3 = name2.split("\\.|\\,");
+    System.out.println(Arrays.toString(name3));
+    String name4 = "Cao Tran Trong Hieu";
+    String[] name5 = name4.split(" ");
+    System.out.println(Arrays.toString(name5));
+    System.out.println("Ten : " + name5[name5.length - 1]);
+  }
 }
