f1ed9e53aa5f2f9e9d2d3662cf8e621723bca22c
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-07-29 14:34:33 +0700

    java

--- Buoi37/ViDuDocText.java
+++ Buoi37/ViDuDocText.java
@@ -5,24 +5,41 @@ import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class ViDuDocText {
     public static void main(String[] args) {
-        File file = new File("E:\\FileTH\\Java\\Buoi36\\file.txt");
+        /*
+         * Cách 1
+         * File file = new File("E:\\FileTH\\Java\\Buoi36\\file.txt");
+         * try {
+         * BufferedReader bufferedReader = Files.newBufferedReader(file.toPath(),
+         * StandardCharsets.UTF_8);
+         * String line = null;
+         * while (true) {
+         * line = bufferedReader.readLine();
+         * if (line == null) {
+         * break;
+         * } else {
+         * System.out.println(line);
+         * }
+         * }
+         * } catch (Exception e) {
+         * e.printStackTrace();
+         * }
+         */
+        /*
+         * Cách 2
+         */
+        File file2 = new File("E:\\\\FileTH\\\\Java\\\\Buoi36\\\\file.txt");
         try {
-            BufferedReader bufferedReader = Files.newBufferedReader(file.toPath(), StandardCharsets.UTF_8);
-            String line = null;
-            while (true) {
-                line = bufferedReader.readLine();
-                if (line == null) {
-                    break;
-                } else {
-                    System.out.println(line);
-                }
+            List<String> allText = Files.readAllLines(file2.toPath(), StandardCharsets.UTF_8);
+            for (String line : allText) {
+                System.out.println(line);
             }
         } catch (Exception e) {
+            // TODO: handle exception
             e.printStackTrace();
         }
-
     }
 }
