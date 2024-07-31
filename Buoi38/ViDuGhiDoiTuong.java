559daf59b5f4dd6197222d7bfd47f0820753bfb3
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-07-31 10:11:29 +0700

    java

---
+++ Buoi38/ViDuGhiDoiTuong.java
@@ -0,0 +1,25 @@
+package Buoi38;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+import Buoi36.Student;
+
+public class ViDuGhiDoiTuong {
+    public static void main(String[] args) {
+        try {
+            File file = new File("E:\\FileTH\\Java\\Buoi38\\file.txt");
+            OutputStream os = new FileOutputStream(file);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(os);
+            SinhVien sinhVien = new SinhVien("98861", "Cao Tran Trong Hieu", 2003, 10);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
+    }
+}
